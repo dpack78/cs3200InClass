fc2723c4efd8913f8a2c4e5c1032fa0cbf22e899
dpack78 <dave78packer@gmail.com>
2018-03-26 13:51:56 -0600

    dp

---
+++ src/styles/Styles.js
@@ -0,0 +1,32 @@
+import { StyleSheet } from 'react-native';
+
+//Main styles for the project
+export default StyleSheet.create({
+    //Centers everything
+    container: {
+      flex: 1,
+      justifyContent: 'center',
+      alignItems: 'center',
+      backgroundColor: '#F5FCFF',
+    },
+    //Styles the text of the button
+    ButtonInner: {
+        fontSize: 50,
+        margin: 3,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#aecbf9',
+    },
+    //Styles the button
+    ButtonOuter: {
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#aecbf9',
+        borderRadius: 10,
+        width: 200,
+        margin: 5,
+    },
+    Link:{
+
+    }
+});
